8617a4d247c93b975722e53ab0653022294d17ba
Cleyton Albuquerque <cleyton.albuquerque@dcx.ufpb.br>
2024-09-16 14:01:13 -0300

    mudança de pastas

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,63 +1,21 @@
 import fastify from "fastify";
 import { serializerCompiler, validatorCompiler, ZodTypeProvider} from "fastify-type-provider-zod";
-import { createGoal } from "../functions/create-goal";
-import z from "zod";
-import { getWeekPendingGoals } from "../functions/get-week-pending-goals";
-import { createGoalCompletion } from "../functions/create-goal-completion";
+
+import { createGoalRoute } from "./routes/create-goals";
+import { createCompletionsRoute } from "./routes/create-completions";
+import { getPendingGoalsRoute } from "./routes/get-pending-goals";
+import { getWeekSummaryRoutes } from "./routes/get-week-sumarry";
+
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler)
 
-//Metas pendentes
-app.get('/pending-goals', async ()=>{
-    const { pendingGoals } = await getWeekPendingGoals()
-    
-    return pendingGoals
-    
-})
-
-// Cadastro de meta
-app.post('/goals',{ 
-    schema: {
-        body: 
-            z.object({
-            title: z.string(),
-            desiredWeeklyFrequency: z.number().int().min(1).max(7),
-            })   
-    }         
-} ,async (request)=>{
-    
-    const {title, desiredWeeklyFrequency} = request.body 
-
-    await createGoal({
-        title,
-        desiredWeeklyFrequency,
-    })
-} )
-
-// Adicionar Metas compridas 
-app.post('/completions', {
-    schema: {
-        body: z.object({
-            goalId: z.string(),
-        })
-    }
-}, async (request)=>{
-    const {goalId} = request.body
-
-    await createGoalCompletion({
-        goalId,
-    })
-    
-} )
-
-
-
-
-
-
+app.register(createGoalRoute)
+app.register(getPendingGoalsRoute)
+app.register(createCompletionsRoute)
+app.register(getWeekSummaryRoutes)
 
 app.listen({
     port: 3333,
